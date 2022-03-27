d48c70b2294df64bcf135128f3083ab4d130a645
SamCo <sac00194@students.stir.ac.uk>
2022-03-27 20:28:37 +0100

    Created a new class called CycleEntry

---
+++ src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
@@ -0,0 +1,7 @@
+package com.stir.cscu9t4practical1;
+
+public class CycleEntry extends Entry {
+    public CycleEntry(String alice, int i, int i1, int i2, int i3, int i4, int i5, int i6, String asphalt, String moderate) {
+        super();
+    }
+}
